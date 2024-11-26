be2b9aebc8deb833ae58e608fc0fddd6d48765b8
mrsan175 <105841100820@student.unismuh.ac.id>
2024-11-26 18:01:43 +0800

    "fix some'

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
 
+const key = "6635faeb0ed57c943fb1a48fa9148787f39120e3783da2b78afbe6d3cd470f0a"
+
 export const generateToken = (user) => {
     const payload = {
         id: user.id,
@@ -7,11 +9,11 @@ export const generateToken = (user) => {
         name: user.name,
         role: user.role,
     };
-    return jwt.sign(payload, process.env.JWT_SECRET, {
+    return jwt.sign(payload, key, {
         expiresIn: '1h',
     });
 };
 
 export const verifyToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET);
+    return jwt.verify(token, key);
 };
